7b4246068de3efe5fce94ed2f95030068ec28dc1
seungyeonkk <seungyeon9319@gmail.com>
2019-07-20 21:49:56 +0900

    [seungyeonkk] 파일업로드 테스트 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MaterialModule} from './material/material.module';
+import { VideoFileComponent } from './video-file/video-file.component';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoFileComponent
   ],
   imports: [
     BrowserModule
     , BrowserAnimationsModule
     , MaterialModule
+    , HttpClientModule
+      , FormsModule
+      , ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
