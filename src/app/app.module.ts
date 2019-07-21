f3aec1659add9891b61966337e27f12ac8a7b9b2
groot <boo8672@naver.com>
2019-07-21 10:23:50 +0900

    feat(router-outlet 설정 추가)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,22 @@ import { MaterialModule} from './material/material.module';
 import { VideoFileComponent } from './video-file/video-file.component';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VideoComponent } from './page/video/detail/video/video.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+    { path: 'video/detail', component: VideoComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
-    VideoFileComponent
+    VideoFileComponent,
+    VideoComponent
   ],
   imports: [
+      RouterModule.forRoot(
+          appRoutes
+      ),
     BrowserModule
     , BrowserAnimationsModule
     , MaterialModule
