538986968e5ffbbdb3079ad065aa2d4c0305a1ba
dong-hyeok shin <sdh910626@gmail.com>
2019-08-22 13:57:11 +0900

    video-upload 완료. video-detail은 합치지 않음. (충돌 날 것 같아서...)

--- src/app/page/video/upload/video-upload/video-upload.component.ts
+++ src/app/page/video/upload/video-upload/video-upload.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { VideoService } from 'src/app/service/video/video.service';
+import { RestService } from 'src/app/service/rest-service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-video-upload',
@@ -11,24 +13,29 @@ import { VideoService } from 'src/app/service/video/video.service';
 export class VideoUploadComponent{
   
 
-  video: any = {
-    title : "",
-    description: "",
-    userId:"test1234",
-    videoFile : null,
-    thumnailFile: null,
-    regDate: "2019-08-01"
+  flag: any = {
+    upload: false
   };
 
+  video: any = {
+      title: ''
+      , description: ''
+      , userId: 'test123'
+      , videoFile: null
+      , thumnailFile: null
+      , regDate: '2019-08-18'
+  };
+  
+  constructor(private restService: RestService, private http: HttpClient) { }
 
-  constructor(private videoService: VideoService) {
-    
+  ngOnInit() {
+
   }
 
   onVideoFileChange(files: FileList) {
-    if (files && files.length > 0) {
-     this.video.videoFile = files[0];
-    }
+      if (files && files.length > 0) {
+      this.video.videoFile = files[0];
+      }
   }
 
   onThumnailFileChange(files: FileList) {
@@ -37,20 +44,26 @@ export class VideoUploadComponent{
       }
   }
 
-
   // video upload
   uploadVideo(): void {
-    console.log(this.video);
-    const formData = new FormData();
-    formData.append('title',this.video.title);
-    formData.append('description',this.video.description);
-    formData.append('userId',this.video.userId);
-    formData.append('thumnailFile',this.video.thumnailFile);
-    formData.append('videoFile',this.video.videoFile);
-    formData.append('regDate',this.video.regDate);
+    this.flag.upload = true;
 
-    this.videoService.uploadVideo(formData).subscribe(res => {
-      console.log(res);
+    const formData: FormData = new FormData();
+    formData.append('title', this.video.title);
+    formData.append('userId', this.video.userId);
+    formData.append('regDate', this.video.regDate);
+    formData.append('description', this.video.description);
+    formData.append('videoFile', this.video.videoFile);
+    formData.append('thumnailFile', this.video.thumnailFile);
+
+    this.restService.postDataByFile('/videos/upload', formData).subscribe(res => {
+        console.log(res);
+        this.flag.upload = false;
+        if (res.result === 'Success') {
+            // TODO 동영상 업로드 목록으로 이동 (회원 마이페이지)
+        }
+    }, error => {
+        this.flag.upload = false;
     });
   }
 }
