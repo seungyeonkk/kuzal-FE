967cec7bfa8e5446ce5121abcac54676c378d321
groot <boo8672@naver.com>
2019-09-07 19:04:27 +0900

    feat : video 조회수 추가

--- src/app/page/video/detail/video/video.component.ts
+++ src/app/page/video/detail/video/video.component.ts
@@ -2,6 +2,10 @@ import {Component, Input, OnInit} from '@angular/core';
 import {VideoService} from '../../../../service/video/video.service';
 import {ActivatedRoute} from '@angular/router';
 import VideoModule from '../../../../model/video';
+import {log} from 'util';
+import {ReplyService} from '../../../../service/reply/reply.service';
+import ReplyModule from '../../../../model/reply';
+import {__await} from 'tslib';
 
 @Component({
     selector: 'app-video',
@@ -12,22 +16,64 @@ import VideoModule from '../../../../model/video';
 export class VideoComponent implements OnInit {
 
     private videoId: string;
+    private login = false;
+    private reply: ReplyModule = new ReplyModule();
 
-    constructor(private route: ActivatedRoute, private videoService: VideoService) {
+    constructor(private route: ActivatedRoute, private videoService: VideoService,
+                private replyService: ReplyService) {
         this.videoId = route.snapshot.paramMap.get('id');
         console.log(this.videoId);
     }
 
     @Input() video: VideoModule = new VideoModule();
+    @Input() replyList: ReplyModule[];
 
 
     ngOnInit() {
         this.getVideo();
+        this.loginCheck();
     }
 
     getVideo(): void {
         const id = this.route.snapshot.paramMap.get('id');
         this.videoService.getVideo(id)
-            .subscribe(video => this.video = video);
+            .subscribe(video => {
+                this.video = video;
+                this.updateViews();
+                this.replyService.getReplyVideoId(video._id).subscribe(
+                    (data: ReplyModule[]) => {
+                        console.log('replyList => ', data);
+                        this.replyList = data;
+                    });
+            });
+    }
+    /*공통함수로 빼야할듯*/
+    loginCheck(): void {
+        if (localStorage.getItem('user')) {
+            this.login = true;
+        }
+    }
+    updateViews(): void {
+        log('updateViews views = ' + this.video.views);
+        this.video.views ++;
+        this.videoService.updateVideoViews(JSON.stringify(this.video))
+            .subscribe(param => log(param));
+    }
+    addReply(): void {
+        log('addReply');
+        if (!this.login) {
+            alert('로그인 후 이용 가능합니다.');
+            return;
+        } else {
+            this.reply.userId = 'test123';
+            this.reply.videoId = this.video._id;
+            this.replyService.addReply(JSON.stringify(this.reply))
+                .subscribe(
+                    (data: ReplyModule[]) => {
+                        console.log('add replyList => ', data);
+                        this.replyList = data;
+                        this.reply = new ReplyModule();
+                    });
+        }
     }
 }
